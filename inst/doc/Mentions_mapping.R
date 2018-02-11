aef8e5faee5ce3514fd63de00fd27ad5f4df1705
llsigerson <llsigerson@gmail.com>
2018-02-11 20:27:20 +0800

    ongoing updates

--- inst/doc/Mentions_mapping.R
+++ inst/doc/Mentions_mapping.R
@@ -1,6 +1,3 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
 ## ---- load packages, include=FALSE---------------------------------------
 library(twittnet)
 library(tibble)
@@ -11,23 +8,23 @@ library(GGally)
 library(scales)
 
 
-## ---- recip_mentioners-----------------------------------------------------------------------------
-MH.mentioners<- recip_mentioners(user="817544938623737858", min.tie= 2, startday=Sys.Date()-30, verbose=F)
+## ---- recip_mentioners---------------------------------------------------
+MH.mentioners<- recip_mentioners(user="817544938623737858", min.tie= 1, startday=Sys.Date()-30)
 
-## ----check mentions--------------------------------------------------------------------------------
+## ----check mentions------------------------------------------------------
 MH.mentioners$mentions[1:3,]
 
-## ----check mentions contacts-----------------------------------------------------------------------
+## ----check mentions contacts---------------------------------------------
 contact.info<- lookup_users(MH.mentioners$mentions$contact)
 contact.info[,c(1,8,9)]
 
-## ----recip_mentioners_network----------------------------------------------------------------------
-MH.network<- recip_mentioners_network("817544938623737858", min.tie=2, startday= Sys.Date()-30)
+## ----recip_mentioners_network--------------------------------------------
+MH.network<- recip_mentioners_network("817544938623737858", min.tie=1, startday= Sys.Date()-30)
 
-## ----check sociogram-------------------------------------------------------------------------------
+## ----check sociogram-----------------------------------------------------
 MH.network$sociogram[1:3,1:3]
 
-## ----prep for vis----------------------------------------------------------------------------------
+## ----prep for vis--------------------------------------------------------
 net<- network(MH.network$sociogram, directed = F)
 edges<- numeric(0)
 for(i in 1:(nrow(MH.network$sociogram)-1)){
@@ -36,7 +33,7 @@ for(i in 1:(nrow(MH.network$sociogram)-1)){
 edges<- as.numeric(edges[edges>0])
 
 
-## ---- summary_plot, fig.height=8-------------------------------------------------------------------
+## ---- summary_plot, fig.height=8, fig.width=10---------------------------
 ggnet2(net, label=T, edge.label = edges, size=20,color="darkblue", label.trim= 3, layout.par = list(cell.jitter = 0.75),   label.color="white", label.size = 10,edge.color = "cadetblue",  
        edge.label.color = "darkblue", edge.label.size = 10, edge.size = log(edges)+.5)
 
